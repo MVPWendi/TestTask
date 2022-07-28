d4f5770ff66816e4517ffd478bb5fe853e90da42
MVPWendi <66933666+MVPWendi@users.noreply.github.com>
2022-07-28 17:39:20 +0800

    Update GetCandidates.sql

--- StoredProcedures/GetCandidates.sql
+++ StoredProcedures/GetCandidates.sql
@@ -1,8 +1,6 @@
---- Процедура получения канидатов, которые еще не выполнили задание, для фонового процесса 
-
 CREATE PROCEDURE GetCandidates
 AS
 	IF (NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Candidates'))
 		EXEC CreateTable
-	SELECT * FROM Candidates WHERE DateWhenCompleteTask IS NULL
+	SELECT * FROM Candidates WHERE DateWhenCompleteTask IS NULL AND YEAR(DateWhenCompleteTask)>2001
 RETURN 0
